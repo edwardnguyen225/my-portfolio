68a5711e717c7fc4f7e4bc8020b4aff16ef9ccf0
Nhan Nguyen <nguyentrinhan@Nhans-MacBook-Air.local>
2024-03-20 20:21:20 +0700

    feat(navbar): First add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarCanvas from "@/components/StarBackground";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       >
         {children}
         <StarCanvas />
+        <Navbar />
       </body>
     </html>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  const sections = [
+    {
+      title: "About",
+      href: "#about-me",
+    },
+    {
+      title: "Skills",
+      href: "#skills",
+    },
+    {
+      title: "Projects",
+      href: "#projects",
+    },
+  ];
+
+  return (
+    <div className="w-full h-[65px] fixed top-0 z-50">
+      <div className="w-full h-full flex flex-row items-center justify-center m-auto">
+        <div className="w-[500px] h-full flex flex-row items-center justify-between">
+          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] px-7 py-[10px] rounded-full text-gray-200 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md">
+            {sections.map((section) => (
+              <Link
+                key={`section-${section.title}`}
+                href={section.href}
+                className="cursor-pointer"
+              >
+                {section.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
