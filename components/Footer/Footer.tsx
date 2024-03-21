e75b473e3bdaaf3c25c3dcd86fb1470979b81485
Nhan Nguyen <nguyentrinhan@Nhans-MacBook-Air.local>
2024-03-21 16:33:33 +0700

    feat(footer): Change about section; Fix align items

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -41,13 +41,8 @@ const columns = [
     links: socialMediaLinks,
   },
   {
-    title: "About",
+    title: "Other Contacts",
     links: [
-      {
-        Icon: FaRegUser,
-        title: "Learning about me",
-        href: "#about-me",
-      },
       {
         Icon: CiMail,
         title: "trinhan.dev@outlook.com",
@@ -61,7 +56,7 @@ const Footer = () => {
   return (
     <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
       <div className="w-full flex flex-col items-center justify-center m-auto">
-        <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
+        <div className="w-full h-full flex flex-row items-start justify-around flex-wrap">
           {columns.map((column) => (
             <div
               key={`column-${column.title}`}
